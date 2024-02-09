492e4c893a0ecf406be1435e357529197c19f8f3
yolo <igoncharov@flightpath3d.com>
2024-02-09 17:34:00 +0000

    chore: configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.cjs', '*.json'],
+  ignorePatterns: ['.eslintrc.cjs', '*.json', '*.md'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
