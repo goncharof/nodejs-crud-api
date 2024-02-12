33a47094305b128db709f8df06dc8e7bee235ea6
yolo <igoncharov@flightpath3d.com>
2024-02-12 16:06:12 +0000

    feat: webpack.config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.cjs', '*.json', '*.md'],
+  ignorePatterns: ['.eslintrc.cjs', '*.json', '*.md', 'webpack.config.cjs'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
