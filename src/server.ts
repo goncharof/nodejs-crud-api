eded0cb34d54a88031a0b0b3c4a86ac6b9b522e5
yolo <igoncharov@flightpath3d.com>
2024-02-12 18:47:52 +0000

    feat: entrypoint index.ts

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,17 @@
 import http from "node:http";
 import { handle } from "./routes/user-routes";
-import "dotenv/config";
 
-const port = process.env.PORT;
+export const server = () => {
+  const server = http.createServer(async (req, res) => {
+    try {
+      const data = await handle(req);
+      res.writeHead(data.status, { "Content-Type": "application/json" });
+      res.end(data.body);
+    } catch (error) {
+      res.writeHead(error.status);
+      res.end(error.message);
+    }
+  });
 
-const server = http.createServer(async (req, res) => {
-  try {
-    const data = await handle(req);
-
-    console.log(data, "data");
-
-    res.writeHead(data.status);
-    res.end(data.body);
-  } catch (error) {
-    res.writeHead(error.status);
-    res.end(error.message);
-  }
-});
-
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  return server;
+};
