d677b89d31d80c0a0a6f2b47e5e13d9c40f8beab
yolo <igoncharov@flightpath3d.com>
2024-02-12 22:28:55 +0000

    feat:  load balancer,  consistent state of db between different workers

--- src/multi-start.ts
+++ src/multi-start.ts
@@ -3,12 +3,20 @@ import { availableParallelism } from "node:os";
 import cluster from "node:cluster";
 import { Server, request } from "node:http";
 import { server } from "./server";
+import { db } from "./utils/database";
 
 const aP = availableParallelism();
 const portMaster = 4000;
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
+
+  cluster.on("message", (w, users) => {
+    db.users = users;
+    for (const id in cluster.workers) {
+      cluster.workers[id].send(db.users);
+    }
+  });
 
   for (let i = 0; i < aP - 1; i++) {
     cluster.fork({ PORT: portMaster + i + 1 });
@@ -21,41 +29,51 @@ if (cluster.isPrimary) {
 
   const workersPorts = [...Array(aP - 1)].map((_, i) => portMaster + i + 1);
   let currentworkersPortsIndex = 0;
-  function getNextPort() {
-    const url = workersPorts[currentworkersPortsIndex];
-    currentworkersPortsIndex =
-      (currentworkersPortsIndex + 1) % workersPorts.length;
-    return url;
-  }
 
   const balancer = createServer((req, res) => {
-    const options = {
-      hostname: "http://localhost",
-      port: getNextPort(),
-      path: req.url,
-      method: req.method,
-      headers: req.headers,
-    };
+    console.log(
+      `balancer will use port ${workersPorts[currentworkersPortsIndex]}`,
+    );
 
-    console.log(options);
-
-    const proxyReq = request(options, (proxyRes) => {
-      res.writeHead(proxyRes.statusCode, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
-    });
-
-    proxyReq.on("error", () => {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      // res.end({ error: err.message });
-    });
-
-    req.pipe(proxyReq, { end: true });
+    req
+      .pipe(
+        request(
+          {
+            path: req.url,
+            port: workersPorts[currentworkersPortsIndex],
+            headers: req.headers,
+            method: req.method,
+          },
+          (r) => {
+            res.writeHead(r.statusCode, r.headers);
+            r.pipe(res);
+          },
+        ),
+      )
+      .on("error", () => {
+        console.log(
+          "balancer failed to connect, try again later, probably worker is not ready",
+        );
+      });
+    currentworkersPortsIndex =
+      (currentworkersPortsIndex + 1) % workersPorts.length;
   });
+
   balancer.listen(portMaster);
 } else {
   const app: Server = server();
 
   app.listen(Number(process.env.PORT), () => {
     console.log(`Server listening on port ${process.env.PORT}`);
+  });
+
+  app.on("request", (req, res) => {
+    res.on("finish", () => {
+      process.send(db.users);
+    });
+  });
+
+  process.on("message", (users: []) => {
+    db.users = users;
   });
 }
