95d056bbbb4d45290492255381e91f1a05bccd18
yolo <igoncharov@flightpath3d.com>
2024-02-12 20:50:46 +0000

    feat: start multiple servers

--- src/multi-start.ts
+++ src/multi-start.ts
@@ -1,3 +1,30 @@
+import { createServer } from "node:http";
 import { availableParallelism } from "node:os";
+import cluster from "node:cluster";
+import { Server } from "http";
+import { server } from "./server"; // Assuming server export
 
-console.log("availableParallelism", availableParallelism());
+const aP = availableParallelism();
+const portMaster = 4000;
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  for (let i = 0; i < aP - 1; i++) {
+    cluster.fork({ PORT: portMaster + i + 1 });
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+
+  const balancer = createServer(() => {});
+  balancer.listen(portMaster);
+} else {
+  const app: Server = server();
+
+  app.listen(Number(process.env.PORT), () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import http from "node:http";
+import { createServer } from "node:http";
 import { handle } from "./routes/user-routes";
 
 export const server = () => {
-  const server = http.createServer(async (req, res) => {
+  const server = createServer(async (req, res) => {
     try {
       const data = await handle(req);
       res.writeHead(data.status, { "Content-Type": "application/json" });
