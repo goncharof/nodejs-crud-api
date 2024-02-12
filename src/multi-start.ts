db7bdff487cced20013bfd91e2814f66a3f12d7a
yolo <igoncharov@flightpath3d.com>
2024-02-12 21:14:28 +0000

    feat: multi servers - working on balancer

--- src/multi-start.ts
+++ src/multi-start.ts
@@ -1,8 +1,8 @@
 import { createServer } from "node:http";
 import { availableParallelism } from "node:os";
 import cluster from "node:cluster";
-import { Server } from "http";
-import { server } from "./server"; // Assuming server export
+import { Server, request } from "node:http";
+import { server } from "./server";
 
 const aP = availableParallelism();
 const portMaster = 4000;
@@ -19,7 +19,38 @@ if (cluster.isPrimary) {
     cluster.fork();
   });
 
-  const balancer = createServer(() => {});
+  const workersPorts = [...Array(aP - 1)].map((_, i) => portMaster + i + 1);
+  let currentworkersPortsIndex = 0;
+  function getNextPort() {
+    const url = workersPorts[currentworkersPortsIndex];
+    currentworkersPortsIndex =
+      (currentworkersPortsIndex + 1) % workersPorts.length;
+    return url;
+  }
+
+  const balancer = createServer((req, res) => {
+    const options = {
+      hostname: "http://localhost",
+      port: getNextPort(),
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+    };
+
+    console.log(options);
+
+    const proxyReq = request(options, (proxyRes) => {
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
+      proxyRes.pipe(res, { end: true });
+    });
+
+    proxyReq.on("error", () => {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      // res.end({ error: err.message });
+    });
+
+    req.pipe(proxyReq, { end: true });
+  });
   balancer.listen(portMaster);
 } else {
   const app: Server = server();
